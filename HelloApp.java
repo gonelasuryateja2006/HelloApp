6284a653abdfb915380073d09b6125320179d636
Your Name <you@example.com>
2026-03-24 23:02:18 +0530

    uc5 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,12 +1,11 @@
-public class HelloApp {
+public class OOPSHelloAppCommaSeparated {
+
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello,SURYA!");
         } else {
-            for (int i = 0; i < args.length; i++) {
-                String name = args[i];
-                System.out.println("hello," + name + "!");
-            }
+            String commaSeparatedNames = String.join(",", args);
+            System.out.println("Hello," + commaSeparatedNames + "!");
         }
     }
 }
