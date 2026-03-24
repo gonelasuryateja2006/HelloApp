55691a998694015217861dbad03fe63b1271cf0a
Your Name <you@example.com>
2026-03-24 22:55:15 +0530

    uc4 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,12 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name = "Surya";
-        if(args.length > 0) {
-        name = args[0]  ;
+        if (args.length == 0) {
+            System.out.println("Hello,SURYA!");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                String name = args[i];
+                System.out.println("hello," + name + "!");
+            }
         }
-        name = name.toUpperCase();
-        System.out.println("Hello," + name + "!");
     }
 }
