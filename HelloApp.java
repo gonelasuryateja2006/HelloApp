8d88635ac318aba870bc4c4adae21372cdc82022
Your Name <you@example.com>
2026-03-31 19:21:27 +0530

    uc7 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
+        
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-            if (nameBuilder.length() > 0) {
-                String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-                System.out.println("Hello, " + names + "!");
-            }
+            System.out.println("Hello, World!");
         }
     }
 }
